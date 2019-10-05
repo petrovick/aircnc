13e771fb04dc6ed2622608717673db5f896b46a1
Macbook <macbook@Macbooks-MacBook-Pro.local>
2019-10-05 16:07:15 -0300

    Image uploads

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
-
+const cors = require("cors");
+const path = require("path");
 mongoose.connect(
   "mongodb+srv://omniuser:omniuser@cluster0-yjuyb.mongodb.net/semana09?retryWrites=true&w=majority",
   {
@@ -16,7 +17,9 @@ const app = express();
 //req.params = Acessar route params (para ediçao, delete)
 //req.body = Acessar corpo da requisicao (para criacao e edicao)
 
+app.use(cors()); //({ origin: "http://localhost:3000" }));
 app.use(express.json());
+app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
 app.use(routes);
 
 app.listen(3333);
